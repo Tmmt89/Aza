e5cf0b8b477dcc47ba77957c62f631c0bfbaac87
tmmt <2222>
2026-08-24 18:03:49 +0300

    Initial Commit

---
+++ Aza/AzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzaApp.swift
+//  Aza
+//
+//  Created by tmmt on 24.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct AzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
